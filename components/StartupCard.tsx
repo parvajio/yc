b467d5c9990171c2a4e74a47ef18bc1bda497b13
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-18 00:11:31 +0600

    image configaration in next.config file and update image in startup card

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,13 +1,15 @@
 import { formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
-const StartupCard = ({post}:{post: startupTypeCard}) => {
+const StartupCard = ({ post }: { post: startupTypeCard }) => {
 
   const {
     _createdAt,
     views,
-    author:{_id: authorId, name},
+    author: { _id: authorId, name },
     title,
     category,
     _id,
@@ -27,8 +29,23 @@ const StartupCard = ({post}:{post: startupTypeCard}) => {
           <EyeIcon className='size-6 text-primary'></EyeIcon>
           <span className='text-16-medium'>{views}</span>
         </div>
+      </div>
 
-        
+      <div className='flex-between mt-5 gap-5'>
+
+        <div className="flex-1">
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+
+        <Link href={`/user/${authorId}`}>
+          <Image alt='placehold' src={'https://placehold.co/600x400'} width={48} height={48}></Image>
+        </Link>
       </div>
     </li>
   )
