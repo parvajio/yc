08b8554d1f42f445c8063caeccebceaac737246b
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-17 23:52:17 +0600

    post distructed

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,17 +3,29 @@ import { EyeIcon } from 'lucide-react'
 import React from 'react'
 
 const StartupCard = ({post}:{post: startupTypeCard}) => {
+
+  const {
+    _createdAt,
+    views,
+    author:{_id: authorId, name},
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
+
   return (
     <li className='startup-card group'>
       <div className='flex-between'>
 
         <p className='startup-card-date'>
-          {formatDate(post?._createdAt)}
+          {formatDate(_createdAt)}
         </p>
 
         <div className='flex gap-1.5'>
           <EyeIcon className='size-6 text-primary'></EyeIcon>
-          <span className='text-16-medium'>{post.views}</span>
+          <span className='text-16-medium'>{views}</span>
         </div>
 
         
